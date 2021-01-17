f0bf46eddd79e356f598424519516407338b7d7c
“Debora <debora.crescenzo@gmail.com>
2021-01-17 15:13:19 +0000

    done

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ImageCard = ({ image }) => {
+  const imageRef = React.createRef();
   const { description, urls } = image;
+  const [span, setSpan] = useState(0);
+
+  useEffect(() => {
+    imageRef.current.addEventListener("load", setSpansFromRef);
+  }, []);
+
+  const setSpansFromRef = () => {
+    const height = imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10); // +1 just to make sure it's rounded up
+    return setSpan(spans);
+  };
 
   return (
-    <div>
-      <img alt={description} src={urls.regular} />
+    <div style={{ gridRowEnd: `span ${span}` }}>
+      <img ref={imageRef} alt={description} src={urls.regular} />
     </div>
   );
 };
